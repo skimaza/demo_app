ac75f10e11f1fe5016513c7ddae7c6b64918bb0f
Stanley Kim <skimaza@gmail.com>
2014-01-07 11:57:04 +0900

    Finish demo app second trial

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module DemoApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Disable this option to run on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
